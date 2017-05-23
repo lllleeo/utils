a18edc8035dea3e9dc6a4b39cdc78a772e024a32
wugengsong <374722806@qq.com>
2017-05-23 23:19:59 +0800

    add: camelCaseToHphenate.js

---
+++ camelCaseToHphenate.js
@@ -0,0 +1,10 @@
+// myButton To: my-button
+function camelCaseToHphenate (str) {
+    var UpCaseArr = str.match(/[A-Z]/g)
+    var result = str
+    UpCaseArr.forEach(function (item) {
+        var low = item.toLowerCase()
+        result = result.replace(item, `-` + low )
+    })
+    return result
+}
