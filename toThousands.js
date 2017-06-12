ada6dedeb725abce5ea8eded7a5158e4fe23dbdd
wugengsong <374722806@qq.com>
2017-06-12 20:07:34 +0800

    add format function

--- toThousands.js
+++ toThousands.js
@@ -1,3 +1,31 @@
 function toThousands(str) {
     return str.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+// https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+/**
+ * Number.prototype.format(n, x)
+ * 
+ * @param integer n: length of decimal
+ * @param integer x: length of sections
+ */
+Number.prototype.format = function(n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
+
+1234..format();           // "1,234"
+12345..format(2);         // "12,345.00"
+123456.7.format(3, 2);    // "12,34,56.700"
+123456.789.format(2, 4);  // "12,3456.79"
